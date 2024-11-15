87f44c537fa8f6adb6ea24f76fb7c85bd07dfa5e
Catherine Njenga <freshiah.catherine@gmail.com>
2024-11-15 19:40:26 +0300

    updating weather details

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import './App.css'
-require('dotenv').config()
+import 'dotenv/config'
 
 const App = () => { 
   const [value, setValue] = useState('')
