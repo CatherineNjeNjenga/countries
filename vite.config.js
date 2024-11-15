aa866c10b252a92df913e7d13528362f2c805c49
Catherine Njenga <freshiah.catherine@gmail.com>
2024-11-15 20:41:41 +0300

    updating icons

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.env.SOME_ENV': `"${import.meta.env.SOME_ENV}"`
+  }
 })
