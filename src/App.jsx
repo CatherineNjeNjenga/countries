d5f6907de2f6560f043a95f011b4f9ee12d5591e
Catherine Njenga <freshiah.catherine@gmail.com>
2024-11-15 21:45:20 +0300

    adding weather icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import './App.css'
-import { config } from 'dotenv'
-config()
 
 const App = () => { 
   const [value, setValue] = useState('')
